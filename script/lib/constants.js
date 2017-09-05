1cc3d82f1bca9858bc1545a05559326d669c202a
Gianluca Schoefer <gianlucaschoefer@gmail.com>
2017-09-05 21:01:52 +0200

    Updated constants

--- script/lib/constants.js
+++ script/lib/constants.js
@@ -1,19 +1,11 @@
-//Declare global abi variables
-var bopFactoryAbi;
-var bopAbi;
+const STATES_MAPPING = {
+  0: 'Open',
+  1: 'Committed',
+  2: 'Expended'
+}
 
-//get JSON data
-$.getJSON('../js/abiDefinitions/bop_factory_abi.json', function(data){
-   bopFactoryAbi = data;
-   const BOP_FACTORY_ABI = bopFactoryAbi;
-});
-
-$.getJSON('../js/abiDefinitions/bop_abi.json', function(data){
-   bopAbi = data;
-});
-
-//intialize global constants from variables
-  
-const BOP_ABI = bopAbi;
-console.log(BOP_FACTORY_ABI);
-const BOP_FACTORY_ADDRESS = '0x1953da4bceBC9579156586C4528aF151381d6Bb6';
+const DEFAULTACTION_MAPPING = {
+  0: 'None',
+  1: 'Release',
+  2: 'Burn'
+}
